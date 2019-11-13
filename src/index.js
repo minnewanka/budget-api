264c7f80cc13d63e7e64bb4c1e2431ca3a3010f1
Philippe Inthavong <philippe.inthavong@bnc.ca>
2019-11-13 12:00:26 -0500

    add delete operation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const User = require('./resolvers/User')
-const Transaction = require('./resolvers/Transaction')
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Transaction = require("./resolvers/Transaction");
 
 const resolvers = {
   Query,
   Mutation,
   User,
   Transaction
-}
+};
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers,
   context: request => {
     return {
       ...request,
-      prisma,
-    }
-  },
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+      prisma
+    };
+  }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,47 +1,52 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const { APP_SECRET, getUserId } = require('../utils')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET, getUserId } = require("../utils");
 
 function post(parent, args, context) {
-  const userId = getUserId(context)
+  const userId = getUserId(context);
   return context.prisma.createTransaction({
     date: args.date,
     title: args.title,
     amount: args.amount,
     proceeded: false,
-    createdBy: { connect: { id: userId } },
-  })
+    createdBy: { connect: { id: userId } }
+  });
+}
+
+function deleteTransaction(parent, args, context) {
+  getUserId(context);
+  return context.prisma.deleteTransaction({
+    id: args.id
+  });
 }
 
 async function signup(parent, args, context, info) {
- 
-  const password = await bcrypt.hash(args.password, 10)
-  const user = await context.prisma.createUser({ ...args, password })
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const password = await bcrypt.hash(args.password, 10);
+  const user = await context.prisma.createUser({ ...args, password });
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
     token,
-    user,
-  }
+    user
+  };
 }
 
 async function login(parent, args, context, info) {
-  
-  const user = await context.prisma.user({ email: args.email })
+  const user = await context.prisma.user({ email: args.email });
   if (!user) {
-    throw new Error('No such user found')
+    throw new Error("No such user found");
   }
 
-  const valid = await bcrypt.compare(args.password, user.password)
+  const valid = await bcrypt.compare(args.password, user.password);
   if (!valid) {
-    throw new Error('Invalid password')
+    throw new Error("Invalid password");
   }
-  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
     token,
-    user,
-  }
+    user
+  };
 }
 
-module.exports = { signup, login, post }
+module.exports = { signup, login, post, deleteTransaction };
